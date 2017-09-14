c5278fc3e82ef21ece7780cc4edc025538791e8b
Geeezzy <Trakir4ik@gmail.com>
2017-09-14 13:17:16 +0300

    add mow.cli#2

--- client/main.go
+++ client/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -41,41 +40,59 @@ func main() {
 			println(string(body))
 		}))
 		//СДЕЛАТЬ ФЛАГ ID
-		cmd.Command("user", "get user by id", cli.ActionCommand(func() {
+		cmd.Command("user", "get user by id", func(sc *cli.Cmd) {
 			//доописать ввод id с клавы
-			res, err := http.Get(DEFAULT_HOST + "/user/" + "2")
-			if err != nil {
-				log.Panic(err)
+			sc.Spec = "[-r] DST "
+
+			var (
+				recursive = sc.BoolOpt("r recursive", false, "Copy files recursively")
+				src       = sc.StringArg("DST", "", "Destination where to copy files to")
+			)
+			sc.Action = func() {
+				fmt.Printf("Copying %s to [recursively: %v ]\n", *src, *recursive)
+				res, err := http.Get(DEFAULT_HOST + "/user/" + *src)
+				if err != nil {
+					log.Panic(err)
+				}
+				body, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					log.Panic(err)
+				}
+				println(string(body))
 			}
-			body, err := ioutil.ReadAll(res.Body)
-			if err != nil {
-				log.Panic(err)
-			}
-			println(string(body))
-		}))
+		})
 
 	})
 
 	//СДЕЛАТЬ ФЛАГ ID
 
 	app.Command("delete", "Delete user", func(cmd *cli.Cmd) {
-		cmd.Command("user", "user by Id", cli.ActionCommand(func() {
+		cmd.Command("user", "user by Id", func(sc *cli.Cmd) {
 			//ввод с клавы
-			client := &http.Client{}
-			req, err := http.NewRequest("DELETE", DEFAULT_HOST+"/user/"+"3", nil)
-			if err != nil {
-				log.Panic(err)
+			sc.Spec = "[-r] DST "
+
+			var (
+				recursive = sc.BoolOpt("r recursive", false, "Copy files recursively")
+				src       = sc.StringArg("DST", "", "Destination where to copy files to")
+			)
+			sc.Action = func() {
+				fmt.Printf("Copying %s to [recursively: %v ]\n", *src, *recursive)
+				client := &http.Client{}
+				req, err := http.NewRequest("DELETE", DEFAULT_HOST+"/user/"+*src, nil)
+				if err != nil {
+					log.Panic(err)
+				}
+				_, err = client.Do(req)
+				if err != nil {
+					log.Panic(err)
+				}
 			}
-			_, err = client.Do(req)
-			if err != nil {
-				log.Panic(err)
-			}
-		}))
+		})
 	})
 
 	app.Command("create", "Create users and ..", func(cmd *cli.Cmd) {
 		//переписать с files.go
-		cmd.Command("user", "create user", (func(sc *cli.Cmd) {
+		cmd.Command("user", "create user", func(sc *cli.Cmd) {
 
 			sc.Spec = "[-r] DST "
 
@@ -85,9 +102,6 @@ func main() {
 			)
 
 			sc.Action = func() {
-				fmt.Printf("%T\n", *src)
-				fmt.Println(len(*src))
-				fmt.Println(*src)
 				fmt.Printf("Copying %s to [recursively: %v ]\n", *src, *recursive)
 
 				bs, err := ioutil.ReadFile(*src)
@@ -95,7 +109,7 @@ func main() {
 					panic(err)
 					return
 				}
-				log.Println("Не соснули")
+
 				b := bytes.NewBuffer(bs)
 
 				res, err := http.Post(DEFAULT_HOST+"/user", "application/json; charset=utf-8", b)
@@ -104,34 +118,43 @@ func main() {
 				}
 				io.Copy(os.Stdout, res.Body)
 			}
-		}))
+		})
 	})
 
 	//СДЕЛАТЬ ФЛАГ ID и ПУТЬ ДО ФАЙЛА
 
 	app.Command("update", "Update  ", func(cmd *cli.Cmd) {
 		//переписать с files.go и ввод id
-		cmd.Command("user", "Update user", cli.ActionCommand(func() {
-			u := User{
-				Name:      "lex",
-				FirstName: "Petr",
-				LastName:  "Moooo",
-			}
-			b := new(bytes.Buffer)
-			json.NewEncoder(b).Encode(u)
+		cmd.Command("user", "Update user", func(sc *cli.Cmd) {
+			sc.Spec = "[-r] DST "
+			var (
+				recursive = sc.BoolOpt("r recursive", false, "Copy files recursively")
+				src       = sc.StringArg("DST", "", "Destination where to copy files to")
+			)
 
-			client := &http.Client{}
-			req, err := http.NewRequest("PUT", DEFAULT_HOST+"/user/"+"2", b)
-			if err != nil {
-				log.Panic(err)
+			sc.Action = func() {
+				bs, err := ioutil.ReadFile(*src)
+				if err != nil {
+					panic(err)
+					return
+				}
+
+				b := bytes.NewBuffer(bs)
+				fmt.Printf("Copying %s to [recursively: %v ]\n", *src, *recursive)
+
+				client := &http.Client{}
+				req, err := http.NewRequest("PUT", DEFAULT_HOST+"/user/"+"2", b)
+				if err != nil {
+					log.Panic(err)
+				}
+				_, err = client.Do(req)
+				if err != nil {
+					log.Panic(err)
+				} else {
+					println("Changes added")
+				}
 			}
-			_, err = client.Do(req)
-			if err != nil {
-				log.Panic(err)
-			} else {
-				println("Changes added")
-			}
-		}))
+		})
 	})
 
 	app.Run(os.Args)
